25ed8f0d346684b77121ec7040a5bd99b65a269c
JoshuaJSR <joshjared2004@gmail.com>
2026-03-09 11:49:16 -0600

    modificacion de navbar usando Angular Materials

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/navbar/navbar";
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatToolbar, MatButton],
+  standalone: true,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
