64c9aa4ef109ac3371f8c735e95971a3a2c77d48
JoshuaJSR <joshjared2004@gmail.com>
2026-03-09 11:01:37 -0600

    comentarios explicacion de creacion de componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from "./pages/home/home";
+import { Navbar } from "./shared/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('static-web');
 }
