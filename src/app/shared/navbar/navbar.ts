5acd13a60883359fb70aaef781a83d0e88ff0401
JoshuaJSR <joshjared2004@gmail.com>
2026-03-10 11:55:27 -0600

    feat: Modificacion colores navbar

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, MatToolbar, MatButton],
+  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
   standalone: true,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
