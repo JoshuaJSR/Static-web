45b2a433b51044a35542cf7489841eb2bfc45679
JoshuaJSR <joshjared2004@gmail.com>
2026-03-11 09:14:11 -0600

    feat: Creacion de carrucel en home.html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
 
+  current = 0;
+  private timer: any;
+
+  slides = [
+    {
+      title: 'Trasloco in sicurezza',
+      text: 'Il tuo trasloco inizia da qui',
+      buttonText: 'Contattaci',
+      buttonLink: '/contact',
+      images: [
+        'images/transporte.webp',
+        'images/exterior.webp',
+        'images/room.webp'
+      ]
+    },
+    {
+      title: 'Hai bisogno di un preventivo?',
+      text: 'Trasparente, veloce e senza impegno',
+      buttonText: 'Richiedi un preventivo',
+      buttonLink: '/quote',
+      images: [
+        'images/cocina.webp',
+        'images/armario.webp',
+        'images/mueble-tv.webp'
+      ]
+    }
+  ];
+
+ ngOnInit() {
+  this.timer = setInterval(() => {
+    this.current = (this.current + 1) % this.slides.length;
+  }, 3000);
+}
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  prev() {
+    this.current = (this.current - 1 + this.slides.length) % this.slides.length;
+  }
+
+  next() {
+    this.current = (this.current + 1) % this.slides.length;
+  }
 }
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
